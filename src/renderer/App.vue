41daea80e72ef643251a01ce0308dec07947e7ae
dj0nny <frank96990@gmail.com>
2019-06-20 14:26:29 +0200

    :sparkles: initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gifsworld'
+  }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+
+body {
+  font-family: 'Montserrat' !important;
+}
+
+</style>
